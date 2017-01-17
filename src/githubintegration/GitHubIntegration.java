65d545e6942d4be7ceb3ffeae76b9458e438f9ad
Luke <Luke@192.168.2.151>
2017-01-17 15:07:12 -0600

    Initial Upload

---
+++ src/githubintegration/GitHubIntegration.java
@@ -0,0 +1,17 @@
+package githubintegration;
+
+/**
+ *
+ * @author Luke
+ */
+public class GitHubIntegration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World.  I made a repository in Github");
+    }
+    
+}
